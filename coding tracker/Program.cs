74d6c9392ea9e5645675d40192d77cc69fe3b12f
Prince-2000-8 <arifulprince2000@gmail.com>
2025-02-12 22:41:42 +0600

    we have many messages

--- coding tracker/Program.cs
+++ coding tracker/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello,c# Academy, one more modification");
+Console.WriteLine("Hello,c# Academy, one more modification"); Console.WriteLine("Hello,c# Academy, one more modification"); Console.WriteLine("Hello,c# Academy, one more modification"); Console.WriteLine("Hello,c# Academy, one more modification");
